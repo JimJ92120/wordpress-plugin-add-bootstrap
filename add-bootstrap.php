3b05b3c553155f2410f6666867fcf27c6251fc3e
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2023-01-19 19:45:13 +0700

    feat(options page): register options page for settings in Appearances tab

--- add-bootstrap.php
+++ add-bootstrap.php
@@ -10,3 +10,22 @@
  * Requires at least:   5.9
  * Requires PHP:        7.4
  */
+
+function render_bootstap_settings_page() {
+    echo <<<HTML
+    <div>
+        <h1>Bootstrap Settings</h1>
+        <p>Edit Bootstrap settings</p>
+    </div>
+HTML;
+}
+
+add_action('admin_menu', function() {
+    add_theme_page(
+        'Bootstrap Settings',
+        'Bootstrap',
+        'edit_theme_options',
+        'bootstrap-settings',
+        'render_bootstap_settings_page'
+    );
+});
