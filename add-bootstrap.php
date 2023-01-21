71529e7f3b2f3e92e224ce5c5fadd0cab7e8fa48
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2023-01-21 14:44:00 +0700

    refactor(options page > fields): replace enable_js field with js view

--- add-bootstrap.php
+++ add-bootstrap.php
@@ -124,22 +124,6 @@ add_action('admin_init', function () {
         ADD_BOOTSTRAP['options']['page_slug'],
         ADD_BOOTSTRAP['options']['section_slug']
     );
-
-    add_settings_field(
-        ADD_BOOTSTRAP['fields']['enable_js'],
-        'Enable JS',
-        function() {
-            $option_value = get_option(ADD_BOOTSTRAP['fields']['enable_js']);
-
-            printf(
-                '<input type="checkbox" id="%1$s" name="%1$s" value="1" %2$s>',
-                ADD_BOOTSTRAP['fields']['enable_js'],
-                $option_value ? 'checked' : ''
-            );
-        },
-        ADD_BOOTSTRAP['options']['page_slug'],
-        ADD_BOOTSTRAP['options']['section_slug']
-    );
 });
 
 add_action('rest_api_init', function () {
--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ import { Button, CheckboxControl } from "@wordpress/components";
 
 const OPTIONS_KEYS = {
   enableCSS: "bootstrap_enable_css",
+  enableJS: "bootstrap_enable_js",
 }
 
 export default function App() {
   const [enableCSS, setEnableCSS] = useEntityProp("root", "site", OPTIONS_KEYS.enableCSS);
+  const [enableJS, setEnableJS] = useEntityProp("root", "site", OPTIONS_KEYS.enableJS);
 
   const { saveEditedEntityRecord } = useDispatch(coreStore);
   const saveOptions = () => {
     saveEditedEntityRecord("root", "site", undefined, {
       [OPTIONS_KEYS.enableCSS]: enableCSS,
+      [OPTIONS_KEYS.enableJS]: enableJS
     });
   };
 
@@ -25,8 +28,13 @@ export default function App() {
       <div>
         <CheckboxControl
           label="Enable CSS"
-          checked={enableCSS | false}
+          checked={enableCSS ? enableJS : false}
           onChange={(newValue) => setEnableCSS(newValue)}
+        />
+        <CheckboxControl
+          label="Enable JS"
+          checked={enableJS ? enableJS : false}
+          onChange={(newValue) => setEnableJS(newValue)}
         />
       </div>
       <Button
